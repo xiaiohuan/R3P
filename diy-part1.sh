10828468397793e4ae79c9c9e575d8aff7811530
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 23:15:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# 只拉取必要插件源，无多余冲突
-echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf.default
+# 添加Argon主题源
+echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
+echo "src-git argonapp https://github.com/jerrykuku/luci-app-argon-config.git" >> feeds.conf.default
+
+# 更新并安装软件源
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-#!/bin/bash
-# 不添加任何第三方插件源，只用LEDE官方源
