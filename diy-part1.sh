a759fc8a2543f697f18dd2772eff1d18fb3d2f12
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 22:41:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# 只拉取必要插件源，无多余冲突
+echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+#!/bin/bash
 # 不添加任何第三方插件源，只用LEDE官方源
