798ee65ce10de97eccf73f0b0c72bca5a2c7dacd
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 23:30:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# 添加Argon主题源
-echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
-echo "src-git argonapp https://github.com/jerrykuku/luci-app-argon-config.git" >> feeds.conf.default
+# 仅添加兼容4.4内核的必要源
+sed -i 's/^src-git/#src-git/g' feeds.conf.default
+echo 'src-git lede https://github.com/coolsnowwolf/lede' >> feeds.conf.default
+echo 'src-git packages https://github.com/openwrt/packages.git^openwrt-21.02' >> feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git^openwrt-21.02' >> feeds.conf.default
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
 
-# 更新并安装软件源
 ./scripts/feeds update -a
 ./scripts/feeds install -a
