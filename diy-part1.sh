3c45e50c096771d1aa3b17167bc4269b4b167112
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 23:43:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# 清空默认feeds
+# 清空默认feeds，避免冲突
 sed -i 's/^src-git/#src-git/g' feeds.conf.default
 
-# 官方基础源 + 兼容最新LEDE的Argon主题源
+# 官方基础源 + Argon主题源
 echo 'src-git lede https://github.com/coolsnowwolf/lede' >> feeds.conf.default
 echo 'src-git packages https://github.com/openwrt/packages.git^master' >> feeds.conf.default
 echo 'src-git luci https://github.com/openwrt/luci.git^master' >> feeds.conf.default
