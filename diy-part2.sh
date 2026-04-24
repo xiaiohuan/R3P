32602c5b8bcbf3704c312010217516eb0c0bac17
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 23:15:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# 修改默认管理IP为 192.168.99.1
+# 修改默认后台管理IP为192.168.99.1
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
-# 设备名改为 新路由3
-sed -i 's/Xiaomi Mi Router 3 Pro/新路由3/g' target/linux/mt7621/base-files/etc/board.d/02_network
-sed -i 's/MI-R3P/Newifi3/g' target/linux/mt7621/base-files/etc/board.d/01_leds
+# 无线性能优化：关闭省电模式，拉满发射功率
+sed -i '/option power_save/d' package/network/config/wifi-files/files/wireless
+sed -i '/txpower/d' package/network/config/wifi-files/files/wireless
+echo -e "option txpower '25'" >> package/network/config/wifi-files/files/wireless
 
-# 无线性能拉满、关闭省电、拉满发射功率
-sed -i '/power_save/d' package/network/config/wifi-files/files/wireless
-echo "option txpower 25" >> package/network/config/wifi-files/files/wireless
-
-# USB3.0 满血优化、解除降频、抗干扰
+# USB3.0满血优化，关闭自动休眠、解除限速
 echo "echo 1 > /sys/kernel/debug/usb/usb3_lpm_disable" >> package/base-files/files/etc/rc.local
 echo "echo 0 > /sys/bus/usb/devices/*/power/autosuspend" >> package/base-files/files/etc/rc.local
 
-# 时区东八区
+# 设置时区为东八区北京时间
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+
+# 强制默认主题为Argon，禁用默认bootstrap主题
+sed -i 's/bootstrap/argon/g' package/base-files/files/etc/config/luci
