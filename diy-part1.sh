ad77075f3e143fe8858cfbbfd59f7c0345c916fd
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 18:28:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# 添加 Passwall 等插件源
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
