af0bf110b798af6b55e035cd0d98c90ae5b5429c
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2026-04-24 23:43:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,11 +5,10 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 # 2. 设置时区为东八区
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
-# 3. USB3.0满血优化（保持不变）
+# 3. USB3.0满血优化
 echo "echo 1 > /sys/kernel/debug/usb/usb3_lpm_disable" >> package/base-files/files/etc/rc.local
 
-# 4. 无线性能优化（换用新的配置方式）
-# 不直接修改不存在的wireless文件，改为在uci-defaults里设置
+# 4. 无线性能拉满（uci-defaults方式，兼容所有版本）
 cat > package/base-files/files/etc/uci-defaults/99-wifi-tune << EOF
 #!/bin/sh
 uci set wireless.radio0.txpower='25'
@@ -20,8 +19,7 @@ uci commit wireless
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-wifi-tune
 
-# 5. 设置默认主题为Argon（换用新的方式）
-mkdir -p package/base-files/files/etc/uci-defaults
+# 5. 默认主题设为Argon
 cat > package/base-files/files/etc/uci-defaults/99-theme << EOF
 #!/bin/sh
 uci set luci.main.mediaurlbase='/luci-static/argon'
@@ -29,3 +27,16 @@ uci commit luci
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-theme
 
+# 6. 开启完整IPv6支持
+cat > package/base-files/files/etc/uci-defaults/99-ipv6 << EOF
+#!/bin/sh
+# 开启LAN IPv6
+uci set network.lan.ipv6='auto'
+uci set network.lan.force_link='1'
+uci set dhcp.lan.dhcpv6='server'
+uci set dhcp.lan.ra='server'
+uci set dhcp.lan.ra_management='1'
+uci commit network
+uci commit dhcp
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-ipv6
